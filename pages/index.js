7350672c76888d36b93055359d20570a176828be
pejoyjp <jinpeng8111765@gmail.com>
2023-01-12 18:28:58 +0000

    second_commit

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,29 @@
 import Head from 'next/head'
-
+import Message from '../components/Message'
+import { useEffect,useState, } from 'react'
+import { db } from '../utils/firebase'
 import { Inter } from '@next/font/google'
+import { async } from '@firebase/util'
+import { collection, onSnapshot, orderBy, query } from 'firebase/firestore'
+import Link from 'next/link'
 
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  //create a state with all the posts
+  const [allPosts,setAllPosts] = useState([])
+  const getPosts = async()=>{
+    const collectionRef = collection(db,'posts')
+    const q = query(collectionRef,orderBy('timestamp','desc'))
+    const unsubscribe = onSnapshot(q,(snapshot)=>{
+      setAllPosts(snapshot.docs.map((doc)=>({...doc.data(),id:doc.id})))
+    })
+    return unsubscribe
+  }
+  useEffect(()=>{
+    getPosts()
+  },[])
   return (
     <div>
       <Head>
@@ -14,8 +32,17 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-      </main>
+      <div className='my-12 text-lg font-medium'>
+        <h2 className='text-2xl'>See What Other people are saying</h2>
+        {allPosts.map((post)=>
+          (<Message {...post} key={post.id}>
+            <Link href={{pathname:`/${post.id}`,query:{...post}}}>
+                <button>{post.comments?.length > 0 ? post.comments : 0}commments</button>
+            </Link>
+          </Message>)
+        )}
+
+      </div>
     </div>
   )
 }
--- pages/post.js
+++ pages/post.js
@@ -1,28 +1,89 @@
 import React from 'react';
 import {auth,db} from "../utils/firebase";
 import {useAuthState} from "react-firebase-hooks/auth";
-import {useRouter} from "next/router";
+import Router, {useRouter} from "next/router";
 import {useEffect,useState} from "react";
+import { addDoc, collection, doc, serverTimestamp, updateDoc } from 'firebase/firestore';
+import { toast } from 'react-toastify';
 
 const Post = () => {
     //form state
     const [post,setPost] = useState({description:""})
+    const [user,loading] = useAuthState(auth)
+    const route = useRouter()
+    const routeData = route.query
+
+
     const submitPost = async (e)=>{
         e.preventDefault()
+        //run checks for des
+        if(!post.description){
+            toast.error("Description Field empty",{
+                position:toast.POSITION.TOP_CENTER,
+                autoClose:1500
+            })
+            return
+        }
+        if(post.description.length > 300){
+            toast.error("Description too long",{
+                position:toast.POSITION.TOP_CENTER,
+                autoClose:1500
+            })
+            return
+        }
+        if(post?.hasOwnProperty('id')){
+            const docRef = doc(db,'posts',post.id)
+            const updatePost = {...post,timestamp:serverTimestamp}
+            await updateDoc(docRef,updatePost)
+            return route.push('/')
+        }else{
+             //make a new post 
+            const collectionRef = collection(db,'posts')
+            await addDoc(collectionRef,{
+                ...post,
+                timestamp:serverTimestamp(),
+                user:user.uid,
+                avatar:user.photoURL,
+                userName:user.displayName
 
+            })
+            setPost({description:""})
+            toast.success('Post has been made!',{position:toast.POSITION.TOP_CENTER,autoClose:1500})
+            return route.push("/")
+
+        }
     }
+
+    //check our user
+    const checkUser = async ()=>{
+        if(loading) return 
+        if(!user) route.push("/auth/login")
+        if(routeData.id){
+            setPost({description:routeData.description,id:routeData.id})
+        }
+    }
+    useEffect(()=>{
+        checkUser()
+    },[user,loading])
+
     return (
         <div className={'my-20 p-12 shadow-lg rounded-lg max-x-md mx-auto'}>
-            <form>
-                <h1 className={'text-2xl font-bold'}>Create a Post</h1>
+            <form onSubmit={submitPost}>
+                <h1 className={'text-2xl font-bold'}>
+                    {post.hasOwnProperty('id')?'Edit your post':'Create your post'}
+                </h1>
                 <div className={'py-2'}>
                     <h3 className={'text-lg font-medium py-2'}>Description</h3>
                     <textarea value={post.description}
-                              onChange={(e)=>setPost(e.target.value)}
+                              onChange={(e)=>setPost({...post,description:e.target.value})}
                               className={'bg-gray-800 h-48 w-full text-white rounded-lg p-2 text-sm'}></textarea>
-                    <p >0/300</p>
+                    <p className= {`text-cyan-600 font-medium text-sm ${post.description.length > 300 ? 'text-red-600':''}`}>{post.description.length}/300</p>
                 </div>
-                <button className={'w-full bg-cyan-500 text-white font-medium p-2 my-2 rounded-lg text-sm'}>Submit</button>
+                <button
+                    type='submit' 
+                    className={'w-full bg-cyan-500 text-white font-medium p-2 my-2 rounded-lg text-sm'}>
+                    Submit
+                </button>
             </form>
 
         </div>
